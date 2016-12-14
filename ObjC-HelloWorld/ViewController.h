7d01cb14bca5349a5a428a14881a582e78984842
Connor Goddard <hello@connorlukegoddard.com>
2016-12-14 20:02:30 +0000

    Updates.

--- ObjC-HelloWorld/ViewController.h
+++ ObjC-HelloWorld/ViewController.h
@@ -8,8 +8,17 @@
 
 #import <UIKit/UIKit.h>
 
+// This is where we declare our "PUBLIC" methods and properties that we want to be able to access from EXTERNAL classes. (i.e. like a '.h' file in C/C++.
+
+// PRIVATE methods/properties are declared in an INTERFACE EXTENSION located within 'ViewController.m'.
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *helloLabel;
+
+// Here we are defining our method DECLARATION.
+- (IBAction)didTapNameButton:(id)sender;
 
 @end
+
+// Note: We can use CTRL-CMD-UP to switch between the '.h' and '.m' files.
 
--- ObjC-HelloWorld/ViewController.m
+++ ObjC-HelloWorld/ViewController.m
@@ -8,11 +8,17 @@
 
 #import "ViewController.h"
 
+// This INTERFACE EXTENSION is where we declare our "PRIVATE" methods and properties that we only want to be accessible INSIDE of the class, but want to be able to call from anywhere in the file.
+
+// This is otherwise known as a 'CLASS CONTINUATION'.
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+
+// 'synthesize' generates an implicit getter and setter for 'helloLabel'.
+@synthesize helloLabel;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -25,5 +31,24 @@
     // Dispose of any resources that can be recreated.
 }
 
+// '-' represents an Instance method (i.e. associated with a particular instance of a class.
+
+// '+' represents a Class method (i.e. can be used with just a class name). This is similar to static method in C#.
+
+// See: http://stackoverflow.com/a/1053646 for more information.
+- (IBAction)didTapNameButton:(id)sender {
+    
+    helloLabel.text = @"Hello Connor!";
+    
+    // '^{}' - Represents a lambda-type function.
+    [UIView animateWithDuration:1.0 animations:^{
+        self.view.backgroundColor = [UIColor redColor];
+        
+        // 'CG'-prefixed functions are 'C' functions (
+        helloLabel.frame = CGRectMake(0, 0, 200, 50);
+    }];
+    
+    
+}
 
 @end
